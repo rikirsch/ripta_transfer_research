9ce58dd4f17b7512cffae14199c48e654e4d978a
rikirsch <rachel_kirsch@brown.edu>
2025-12-09 06:34:59 -0500

    updating storage to use R Script folder

--- R Scripts/Test.R
+++ R Scripts/Test.R
@@ -1,19 +1,65 @@
 #To test out my functions
 
-#can my functions run on scheduled and actual arrival times?
-#read in the full dataframe
-otp_df <- read.csv('/Users/rachelkirsch/Downloads/1560FinalProject/otp_simulated.csv')
+# The following commented out code chunk will produce an error if run because
+#it was created to run on dataframes that do not have longitude or latitude in the data
 
-#clean the data based on the actual arrival times and calculate the number of transfers to route 10 
-actual_otp_df <- clean_data(otp_df, "Actual.Arrival.Time", "2024-05-01")
-act_resulting_df <- route_transfers(actual_otp_df, route_num = 10, from = FALSE)
+# #can my functions run on scheduled and actual arrival times?
+# #read in the full dataframe
+# otp_df <- read.csv('/Users/rachelkirsch/Downloads/1560FinalProject/otp_simulated.csv')
+# 
+# #clean the data based on the actual arrival times and calculate the number of transfers to route 10 
+# actual_otp_df <- clean_data(otp_df, "Actual.Arrival.Time", "2024-05-01")
+# act_resulting_df <- route_transfers(actual_otp_df, route_num = 10, from = FALSE)
+# 
+# #clean the data based on the scheduled arrival times and calculate the number of transfers to route 10 
+# sched_otp_df <- clean_data(otp_df, "Scheduled.Time", "2024-05-01")
+# sched_resulting_df <- route_transfers(sched_otp_df, route_num = 10, from = FALSE)
+# 
+# #trying to run the cleaning function on a day of the week
+# sched_mon_df <- clean_data(otp_df, "Scheduled.Time", "Monday")
+# #data cleaning on day of the week and actual arrival time
+# act_mon_df <- clean_data(otp_df, "Actual.Arrival.Time", "Monday")
+# #calculate the transfers for the more advanced dataframe 
+# #(day of week and actual arrival time)
+# res_act_mon_transfers <- route_transfers(act_mon_df, route_num = 10)
 
-#clean the data based on the scheduled arrival times and calculate the number of transfers to route 10 
-sched_otp_df <- clean_data(otp_df, "Scheduled.Time", "2024-05-01")
-sched_resulting_df <- route_transfers(sched_otp_df, route_num = 10, from = FALSE)
 
-#trying to run the cleaning function on a day of the week
-sched_mon_df <- clean_data(otp_df, "Scheduled.Time", "Monday")
+#The following commented out chunk of code no longer runs because
+#I have updated the data structure of my program and the dependence between scripts
 
-act_mon_df <- clean_data(otp_df, "Actual.Arrival.Time", "Monday")
-res_act_mon_transfers <- route_transfers(act_mon_df, route_num = 10)
+#Run the data cleaning function and the transfer calculator function on the csv
+#with latitude and longitudinal data
+otp_long_ltd <- read.csv('/Users/rachelkirsch/Downloads/1560FinalProject/otp_sim_lat_long.csv')
+# ripta_df_long_lat <- clean_data(otp_long_ltd, "Actual.Arrival.Time", "Thursday")
+# res_long_lat <- route_transfers(ripta_df_long_lat, route_num = 10, from = FALSE, day = "Thursday")
+# 
+# sun_ripta_df_long_lat <- clean_data(otp_long_ltd, "Actual.Arrival.Time", "Sunday")
+# sun_route_transfers(ripta_df_long_lat, route_num = 10, from = FALSE, day = "Sunday")
+# 
+# mon_ripta_df_long_lat <- clean_data(otp_long_ltd, "Actual.Arrival.Time", "Monday")
+# mon_route_transfers(ripta_df_long_lat, route_num = 10, from = FALSE, day = "Sunday")
+
+
+
+#Running the clean_data function now that it calls route_transfers which calls plotting_transfers
+thurs_rt10_transf_plot <- clean_data(otp_long_ltd,
+                                     type_of_time = "Actual.Arrival.Time", 
+                                     day = "Tuesday", 
+                                     route_number = 10, 
+                                     transfer_wait_time = 15, 
+                                     from_to = TRUE)
+ggsave("/Users/rachelkirsch/Downloads/1560FinalProject/ripta_transfer_research/Results/Sunday_Actual_Rt10_Transfer_Plot.png", 
+       plot = thurs_rt10_transf_plot,
+       width = 7, height = 7)
+
+mon_rt11_transf_plot <- clean_data(otp_long_ltd,
+                                   type_of_time = "Actual.Arrival.Time", 
+                                   day = "Monday", 
+                                   route_number = 11, 
+                                   transfer_wait_time = 15, 
+                                   from_to = TRUE)
+ggsave("/Users/rachelkirsch/Downloads/1560FinalProject/ripta_transfer_research/Results/mon_actual_rt11_transf_plot.png", 
+       plot = mon_rt11_transf_plot,
+       width = 7, height = 7)
+
+sapply(otp_long_ltd$Route, clean_data)
--- R Scripts/prepare_data.R
+++ R Scripts/prepare_data.R
@@ -1,6 +1,8 @@
 #source(find_num_transfers)
+#source(Plotting)
 library(tidyverse)
 library(lubridate)
+library(ggplot2)
 library(hms)
 
 #' Cleaning the Initial Data
@@ -15,7 +17,12 @@ library(hms)
 #' @param day chr, the date that the number of transfers is calculated for
 #' @return cleaned_data dataframe of polished and complete bus routes used to 
 #' calculate the number of transfers for the bus route on the specified day
-clean_data <- function(full_data, type_of_time, day){
+clean_data <- function(full_data, 
+                       type_of_time, 
+                       day, 
+                       route_number, 
+                       transfer_wait_time = 15, 
+                       from_to = TRUE){
   cleaned_data <- full_data %>%
     #mutate all dates and times as needed
     mutate(Date = as.Date(Date),
@@ -24,28 +31,35 @@ clean_data <- function(full_data, type_of_time, day){
            Time = type_of_time)
   
   #check to see if finding the trasfers for a specific date or for a day of the week
-    if(day %in% c("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")){
-      #filter to the specified day of the week
-      cleaned_data <- cleaned_data %>%
-        filter(Weekday == day)
-    } else{
-      #filter to the specified date
-      cleaned_data <- cleaned_data %>%
-        filter(Date == day)
-    }
+  if(day %in% c("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")){
+    #filter to the specified day of the week
+    cleaned_data <- cleaned_data %>%
+      filter(Weekday == day)
+  } else{
+    #filter to the specified date
+    cleaned_data <- cleaned_data %>%
+      filter(Date == day)
+  }
   
   #select the columns of interest
   cleaned_data <- cleaned_data %>%
-    select(c(Date, Route, Stop, Stop.Sequence, Time))
- 
+    select(c(Date, Route, Stop, Stop.Sequence, Time, StopLat, StopLng))
+  
   #Update time to be the average arrival time on each route and stop
   cleaned_data <- cleaned_data %>%
     #group by parameters of interest, hour is used to differentiate between
     #the multiple times that a route is run a day
-    group_by(hour(Time), Route, Stop, Stop.Sequence) %>%
+    group_by(hour(Time), Route, Stop, Stop.Sequence, StopLat, StopLng) %>%
     #calculate the average arrival time
     summarize(Time = as.POSIXct(as_hms(mean(Time))))    %>%
     ungroup()
-   
-  return(cleaned_data)
+  
+  #return(cleaned_data)
+  res <- route_transfers(cleaned_data,
+                         route_number,
+                         transfer_wait_time,
+                         from = from_to, day)
+  return(res)
 }
+
+#I want to group by every 20 minutes instead of every hour to try and account for some of the weird grouping that is happening
